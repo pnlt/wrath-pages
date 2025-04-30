ddc06c91b887359a7595dc80326f486aa240049e
𝑾𝒊𝒍𝒍𝒊𝒂𝒎 𝑱𝒂𝒎𝒆𝒔 𝑴𝒐𝒓𝒊𝒂𝒓𝒕𝒚 <pnlt170603@gmail.com>
2025-04-30 07:51:34 +0700

    style: :lipstick: change images && modify some scripts

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,20 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Wrath's homepage" />
+        <meta name="author" content="Wrath Team" />
+        <meta name="author" content="Wrath" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <link rel="shortcut icon" href="/favicon3.ico" type="image/x-icon" />
+        <meta name="twitter:title" content="Wrath Team" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:site" content="@wrath" />
+        <meta name="twitter:creator" content="@wrath" />
+        <meta name="twitter:image" content="/card.png" />
+        <meta property="og:site_name" content="Wrath's homepage" />
+        <meta name="og:title" content="Wrath Team" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="/card.png" />
         <title>Wrath - Homepage</title>
       </Head>
 
